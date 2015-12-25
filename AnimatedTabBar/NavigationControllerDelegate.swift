71c7ba5caab05e67417760d124a258c83d55492f
Franklin Schrans <fs2014@ic.ac.uk>
2015-12-25 12:10:14 +0100

    Removed commented out NavigationControllerDelegate

--- AnimatedTabBar/NavigationControllerDelegate.swift
+++ AnimatedTabBar/NavigationControllerDelegate.swift
@@ -8,12 +8,6 @@
 
 import UIKit
 
-//class NavigationControllerDelegate: NSObject, UINavigationControllerDelegate {
-//    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
-//        return MoveTransitionAnimator()
-//    }
-//}
-
 class TabBarControllerDelegate: NSObject, UITabBarControllerDelegate {
     @IBOutlet weak var tabBarController: UITabBarController!
     
