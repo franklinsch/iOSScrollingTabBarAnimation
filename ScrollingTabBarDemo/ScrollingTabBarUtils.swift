88129bb185209ffc177f22ac14ffb39cb15d8ca8
stevesb <stevesuadb@gmail.com>
2017-01-01 16:10:02 -0600

    Updated to Swift 3.0.1

--- ScrollingTabBarDemo/ScrollingTabBarUtils.swift
+++ ScrollingTabBarDemo/ScrollingTabBarUtils.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ScrollingTabBarControllerDelegate: NSObject, UITabBarControllerDelegate {
-    func tabBarController(tabBarController: UITabBarController, animationControllerForTransitionFromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    func tabBarController(_ tabBarController: UITabBarController, animationControllerForTransitionFrom fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         return ScrollingTransitionAnimator(tabBarController: tabBarController, lastIndex: tabBarController.selectedIndex)
     }
 }
@@ -19,7 +19,7 @@ class ScrollingTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioni
     var tabBarController: UITabBarController!
     var lastIndex = 0
     
-    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return 0.4
     }
     
@@ -28,29 +28,29 @@ class ScrollingTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioni
         self.lastIndex = lastIndex
     }
     
-    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         self.transitionContext = transitionContext
         
-        let containerView = transitionContext.containerView()
-        let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)
-        let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)
+        let containerView = transitionContext.containerView
+        let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)
+        let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)
         
-        containerView?.addSubview(toViewController!.view)
+        containerView.addSubview(toViewController!.view)
         
-        var viewWidth = CGRectGetWidth(toViewController!.view.bounds)
+        var viewWidth = toViewController!.view.bounds.width
         
         if tabBarController.selectedIndex < lastIndex {
             viewWidth = -viewWidth
         }
         
-        toViewController!.view.transform = CGAffineTransformMakeTranslation(viewWidth, 0)
+        toViewController!.view.transform = CGAffineTransform(translationX: viewWidth, y: 0)
         
-        UIView.animateWithDuration(self.transitionDuration((self.transitionContext)), delay: 0.0, usingSpringWithDamping: 1.2, initialSpringVelocity: 2.5, options: .TransitionNone, animations: {
-            toViewController!.view.transform = CGAffineTransformIdentity
-            fromViewController!.view.transform = CGAffineTransformMakeTranslation(-viewWidth, 0)
-            }, completion: { _ in
-                self.transitionContext?.completeTransition(!self.transitionContext!.transitionWasCancelled())
-                fromViewController!.view.transform = CGAffineTransformIdentity
+        UIView.animate(withDuration: self.transitionDuration(using: (self.transitionContext)), delay: 0.0, usingSpringWithDamping: 1.2, initialSpringVelocity: 2.5, options: .overrideInheritedOptions, animations: {
+            toViewController!.view.transform = CGAffineTransform.identity
+            fromViewController!.view.transform = CGAffineTransform(translationX: -viewWidth, y: 0)
+        }, completion: { _ in
+            self.transitionContext?.completeTransition(!self.transitionContext!.transitionWasCancelled)
+            fromViewController!.view.transform = CGAffineTransform.identity
         })
     }
 }
